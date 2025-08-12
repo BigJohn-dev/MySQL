1d2380c9c6d0c54e74ebe0fdd81cf8267a0c0764
John Imeobong <imeobongjohn38@gmail.com>
2025-08-12 07:41:05 +0100

    another commit

--- Chapter 2/Home Task/Task three/Marcia’s Dry Cleaning.sql
+++ Chapter 2/Home Task/Task three/Marcia’s Dry Cleaning.sql
@@ -40,5 +40,99 @@ WHERE lastName REGEXP 'cat';
 -- Question 7
 SELECT 	LastName, FirstName, Phone
 FROM customer
-WHERE phone LIKE '_23%'
--- WHERE SUBSTRING(phone, 2, 2) = '23';
+WHERE phone LIKE '_23%';
+
+-- Question 8
+SELECT MAX(TotalAmount) AS Max_TotalAmount,
+		MIN(TotalAmount) AS MinTotalAmount
+FROM Invoice;
+
+-- Question 9
+SELECT AVG(TotalAmount) AS Average_TotalAmount
+FROM Invoice;
+
+-- Question 10
+SELECT COUNT(*) AS Number_Of_Customers
+FROM customer;
+
+-- Question 11
+SELECT lastName
+FROM customer
+GROUP BY lastName;
+
+SELECT firstName
+FROM customer
+GROUP BY firstName;
+
+-- Question 12
+SELECT COUNT(firstName) AS First_name, COUNT(lastName) AS Last_name
+FROM customer;
+
+-- Question 13
+SELECT LastName, FirstName, Phone
+FROM customer
+WHERE CustomerID IN (
+    SELECT CustomerID
+    FROM invoice
+    WHERE TotalAmount > 100.00
+)
+ORDER BY LastName, FirstName DESC;
+
+-- Question 14
+SELECT 
+    customer.lastName, 
+    customer.firstName,
+    customer.phone
+FROM customer
+WHERE customer.CustomerID IN (
+    SELECT invoice.CustomerID
+    FROM invoice
+    WHERE invoice.TotalAmount > 100.00
+)
+ORDER BY customer.lastName, customer.firstName DESC;
+
+-- Question 15
+SELECT 
+    customer.lastName, 
+    customer.firstName,
+    customer.phone
+FROM customer
+JOIN invoice ON customer.CustomerID = invoice.CustomerID
+WHERE invoice.TotalAmount > 100.00
+ORDER BY customer.LastName, customer.FirstName DESC;
+
+-- Question 16
+SELECT 
+    customer.lastName, 
+    customer.firstName,
+    customer.phone
+FROM customer
+WHERE customer.CustomerID IN (
+    SELECT invoice_item.itemNumber
+    FROM invoice_item
+    WHERE invoice_item.item = 'Dress Shirt'
+)
+ORDER BY customer.lastName, customer.firstName DESC;
+
+-- Question 17
+SELECT 
+    customer.lastName, 
+    customer.firstName,
+    customer.phone
+FROM customer
+WHERE customer.CustomerID IN (
+    SELECT invoice_item.itemNumber
+    FROM invoice_item
+    WHERE invoice_item.item = 'Dress Shirt'
+)
+ORDER BY customer.lastName, customer.firstName DESC;
+
+-- Question 18
+SELECT 
+    customer.LastName, 
+    customer.FirstName,
+    customer.Phone
+FROM customer
+JOIN invoice ON customer.CustomerID = invoice.itemNumber
+WHERE invoice_item.item = 'Dress Shirt'
+ORDER BY customer.lastName, customer.firstName DESC;
